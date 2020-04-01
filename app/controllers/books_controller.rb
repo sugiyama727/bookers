e2c4effbb8320fe043f6a29f0dc2a68e4ad7cdb6
杉山　ちひろ <ska69nknk@icloud.com>
2020-04-01 15:05:26 +0900

    Bookers-1

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book, notice:'Book was successfully created.'
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      redirect_to book_path(book.id), notice:'Book was successfully updated.'
+    else
+      @book = Book.find(params[:id])
+      render :show
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+    redirect_to '/books', notice:'Book was successfully destroyed.'
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+
+  root to: 'books#top'
+
+  post 'books' => 'books#create'
+
+  patch 'books/:id' => 'books#update', as: 'update_book'
+
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+end
